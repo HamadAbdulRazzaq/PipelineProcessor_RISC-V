f92da44f2781111805cabd59e5b39c52498a59b8
unknown <hr06899@st.habib.edu.pk>
2022-04-25 20:00:47 +0500

    Insertion Sort ASM Finalized

--- Insertion Sort/insertion_sort.asm
+++ Insertion Sort/insertion_sort.asm
@@ -23,8 +23,8 @@ addi x9,  x0, 75
 SW   x9,  36(x10)
 # Array Initialization Done
 #----Sorting via insertion sort----#
-addi x1,x0,4 					# j = 1 {x1 = j}
-blt  x1,x10,Outer_Loop 			# if j < len(A) -> Start the loop
+addi x1, x0, 4 					# j = 1 {x1 = j}
+blt  x1, x10, Outer_Loop 		# if j < len(A) -> Start the loop
 Outer_Loop:
 add  x3, x1, x10 				# j + base address
 LW   x4, 0(x3) 					# key = A[j] {x4 = A[j]}
